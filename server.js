7e6fad575a93af9a40c60dd8a574a5286fe4c7b1
Driss Boumlik <boumlik.t@gmail.com>
2018-12-20 13:21:54 +0100

    FEATURE: Setting up the server

---
+++ server.js
@@ -0,0 +1,33 @@
+// HTTP Module for Creating Server and Serving Static Files Using Node.js
+// Static Files: HTML, CSS, JS, Images
+// Get Complete Source Code from Pabbly.com
+
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var port = 5000;
+http.createServer(function(req, res){
+
+    if(req.url === "/"){
+        fs.readFile("./index.html", "UTF-8", function(err, html){
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(html);
+        });
+    }else if(req.url.match("\.css$")){
+        var cssPath = path.join(__dirname, '/', req.url);
+        var fileStream = fs.createReadStream(cssPath, "UTF-8");
+        res.writeHead(200, {"Content-Type": "text/css"});
+        fileStream.pipe(res);
+
+    }else if(req.url.match("\.js$")){
+        var imagePath = path.join(__dirname, '/', req.url);
+        var fileStream = fs.createReadStream(imagePath);
+        res.writeHead(200, {"Content-Type": "text/js"});
+        fileStream.pipe(res);
+    }else{
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end("No Page Found");
+    }
+
+}).listen(port);
+console.log('server started: http://127.0.0.1:'+port);
